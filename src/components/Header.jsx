a08d327e62a08699a7fef3ea2022167266b36ccb
amihan <amirkhan.shamgonov.0101@mail.ru>
2022-05-08 16:53:09 +0600

    12urok

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return(
+        <header className="header">
+                <img className="header__img" src="https://i.pinimg.com/originals/f6/7a/3a/f67a3a261f0c952640f9c1d214d73e96.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img className="content__img" src="https://cdn.pixabay.com/photo/2017/01/07/12/39/sea-1960262_1280.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                    <div>
+                        Post 3
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
