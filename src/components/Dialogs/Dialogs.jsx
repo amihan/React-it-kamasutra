9925e361b58fd4a8136b364b09a252966188add2
amihan <amirkhan.shamgonov.0101@mail.ru>
2022-05-11 19:17:56 +0600

    21 urok

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,28 @@ import s from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
-            Dialogs
+            <div className={s.dialogs__items}>
+                <div className={s.dialogs__item}>
+                    Amir
+                </div>
+                <div className={s.dialogs__item}>
+                    Sasha
+                </div>
+                <div className={s.dialogs__item}>
+                    Urod
+                </div>
+            </div>
+            <div className={s.dialogs__messages}>
+                <div className={s }>
+                    Hi
+                </div>
+                <div className={s.dialogs__message}>
+                    Ha
+                </div>
+                <div className={s.dialogs__message}>
+                    Urod
+                </div>
+            </div>
         </div>
     )
 }
