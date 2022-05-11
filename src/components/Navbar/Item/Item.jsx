00300da51b4ad5470ba8e1f629ecb5e756729547
amihan <amirkhan.shamgonov.0101@mail.ru>
2022-05-11 18:20:29 +0600

    20 urok - navlink

--- src/components/Navbar/Item/Item.jsx
+++ src/components/Navbar/Item/Item.jsx
@@ -1,10 +1,11 @@
 import s from './Item.module.css'
+import {NavLink} from "react-router-dom";
 
 const Item = (props) => {
     return (
         <div className={s.item}>
             <img src="" alt=""/>
-            <a href={props.source}>{props.value}</a>
+            <NavLink to={props.source} className = { navData => navData.isActive ? s.active : s.item }>{props.value}</NavLink>
         </div>
     )
 }
